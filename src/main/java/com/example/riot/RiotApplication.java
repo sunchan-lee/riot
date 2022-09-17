114c9f0253b75fde5e458db49f6982331c2ee657
Sun Chan Lee <79978556+sunchan-lee@users.noreply.github.com>
2022-09-17 20:29:31 +0900

    Add files via upload

---
+++ src/main/java/com/example/riot/RiotApplication.java
@@ -0,0 +1,13 @@
+package com.example.riot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiotApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/riot/key.java
@@ -0,0 +1,8 @@
+package com.example.riot;
+
+public class key {
+
+    //riot developers 에서 제공하는 API KEY
+    private String api_key = "RGAPI-376d7b5d-9f9a-49fa-8d18-86182156ca0d";
+    
+}
